6e008454d6ccceb0ef0be2659b75ad4ffa09e663
Maxim <prisyazhnuy@gmail.com>
2014-02-26 22:31:40 +0200

    Change maxlenght

--- src/main/java/org/util/rtc/annotation/maxlength.java
+++ src/main/java/org/util/rtc/annotation/maxlength.java
@@ -1,6 +1,7 @@
 package org.util.rtc.annotation;
 
-
+@java.lang.annotation.Target({java.lang.annotation.ElementType.FIELD})
+@java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 public @interface maxlength {
     int value();
 }
