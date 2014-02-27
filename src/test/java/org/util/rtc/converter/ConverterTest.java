421f1088367451033621c7514ddb99d2d150ed72
alex9229 <mika9229@mail.ru>
2014-02-27 17:54:02 +0300

    Update ConverterTest.java

--- src/test/java/org/util/rtc/converter/ConverterTest.java
+++ src/test/java/org/util/rtc/converter/ConverterTest.java
@@ -2,13 +2,13 @@ package org.util.rtc.converter;
 
 import org.junit.Test;
 import org.util.rtc.entity.User;
-
+import org.util.rtc.converter.*;
 /**
  * Created by Dell on 27.02.14.
  */
 public class ConverterTest {
     @Test
     public void testToJSON() throws Exception {
-        Converter.toJSON(new User());
+        Assert.assertTrue(Converter.toJSON(user)).equals("{"rules":{"age":{"min":"10","maxlength":"20"},"name":{"minlength":"5"}}}");
     }
 }
