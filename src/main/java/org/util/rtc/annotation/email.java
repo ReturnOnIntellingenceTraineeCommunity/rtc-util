5a5d38264c5208fdb3de5155d910445f4f2cdfde
Maxim <prisyazhnuy@gmail.com>
2014-02-27 15:16:54 +0200

    Finish annotations

--- src/main/java/org/util/rtc/annotation/email.java
+++ src/main/java/org/util/rtc/annotation/email.java
@@ -1,6 +1,6 @@
 package org.util.rtc.annotation;
 
-@java.lang.annotation.Target({java.lang.annotation.ElementType.FIELD})
+@java.lang.annotation.Target(java.lang.annotation.ElementType.FIELD)
 @java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 public @interface email {
 }
--- src/main/java/org/util/rtc/annotation/minlength.java
+++ src/main/java/org/util/rtc/annotation/minlength.java
@@ -1,6 +1,6 @@
 package org.util.rtc.annotation;
 
-@java.lang.annotation.Target({java.lang.annotation.ElementType.FIELD})
+@java.lang.annotation.Target(java.lang.annotation.ElementType.FIELD)
 @java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.CLASS)
 public @interface minlength {
     int value();
