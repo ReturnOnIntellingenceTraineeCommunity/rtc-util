f4d1322d6f33c7e715789b5f8556125d41f9d409
ivanYatcuba <ivan.yatsouba@gmail.com>
2014-04-30 21:06:14 +0300

    validation context constructor and field name updates

--- src/test/java/net/github/rtc/util/AnnotatedFieldsScannerTest.java
+++ src/test/java/net/github/rtc/util/AnnotatedFieldsScannerTest.java
@@ -1,26 +1,17 @@
 package net.github.rtc.util;
 
-import net.github.rtc.util.converter.AnnotatedFieldScanner;
-import net.github.rtc.util.entities.User;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.lang.annotation.Annotation;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by ivan on 28.04.14.
  */
 @Ignore
 public class AnnotatedFieldsScannerTest {
-    @Autowired
-    AnnotatedFieldScanner scanner;
+
     @Test
     public void testScanner() throws Exception {
-        Map<String, List<Annotation>> fields = scanner.scan(User.class, "");
-        System.out.println(fields);
+
     }
 
 
